7f06e8a19cc33ca23167557bfc8c476ea101dc35
mathematical-algorithm-3478 <jlboyer1@gmail.com>
2019-05-13 16:40:32 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,6 @@
 # Write your #display_rainbow method here
 
+=begin
 def display_rainbow(colorArr)
   stringRet = ""
   colorArr.each do |color|
@@ -7,5 +8,18 @@ def display_rainbow(colorArr)
   end
   puts stringRet[0,stringRet.length-2]
 end
+=end
+
+def display_rainbow(colors)
+  print "R: #{colors[0]}, "
+  print "O: #{colors[1]}, "
+  print "Y: #{colors[2]}, "
+  print "G: #{colors[3]}, "
+  print "B: #{colors[4]}, "
+  print "I: #{colors[5]}, "
+  print "V: #{colors[6]}, "
+end
+  
+
 
     
